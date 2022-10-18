fff8c52b2199a7a79566f6967c7548d59fbae6e9
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2022-10-18 15:07:10 +0800

    feature: add popup close button

--- src/components/CharacterPopup.tsx
+++ src/components/CharacterPopup.tsx
@@ -8,11 +8,16 @@ interface Info {
 	img: string
 }
 
+interface CloseButtonProps {
+	onClose: () => void
+}
+
 export default function CharacterCard(props: Info) {
 
 	return (
     <Popup open={props.isOpen} closeOnDocumentClick onClose={props.onClose}>
     	<Card sx={{ opacity: 0.9 }}>
+    		<CloseButton onClose={props.onClose} />
     		<CardMedia
 	        component="img"
 	        image={props.img}
@@ -21,4 +26,27 @@ export default function CharacterCard(props: Info) {
     	</Card>
     </Popup>
    )
+}
+
+function CloseButton(props: CloseButtonProps) {
+    return (
+    	<button
+    		style={{ 
+    			position: 'absolute',
+    			width: '50px', 
+    			height: '50px',
+    			border: 'none', 
+    			backgroundColor: 'white',
+    			opacity: 0.9,
+    			fontSize: '30px',
+    			// borderRadius: '50%',
+    			margin: '0', 
+    			padding: '10px',
+    			cursor: 'pointer'
+    		}}
+    		onClick={props.onClose}
+    	>
+    		&times;
+    	</button>
+  	)
 }
